38d6309285af3589149a412169ed3fa5679bd9b4
Camille <cfghjg.qsdfhksqdfvbn@outlook.com>
2025-04-29 14:08:54 +0200

    Update home.js

--- converter/home.js
+++ converter/home.js
@@ -6,3 +6,13 @@ document.querySelector("button[title=\"fermer\"]").addEventListener("click", () 
     document.querySelector("dialog.converter").close();
     document.querySelector("button.conver-download").removeAttribute("hidden");
 });
+document.querySelector("button[title=\"fermer la fenêtre\"]").addEventListener("click", () => {
+    document.querySelector("dialog.converter").close();
+    document.querySelector("button.conver-download").removeAttribute("hidden");
+});
+document.querySelector("a[href=\"/instal/converter/Video Covert_Tools.exe\"]").addEventListener("click", () => {
+    document.querySelector("input.check").checked
+})
+document.querySelector("a[href=\"/instal/converter/AMVConverter.zip\"]").addEventListener("click", () => {
+    document.querySelector("input.check").checked
+})
